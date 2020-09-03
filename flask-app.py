44309cf1090e988fcac36e37bc55c5a9e2fc9263
ayushbasak <ayushbasak0210@gmail.com>
2020-09-03 13:55:11 +0530

    Final CSS complete.

--- flask-app.py
+++ flask-app.py
@@ -35,5 +35,9 @@ def home():
     con.commit()
     con.close()
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(host = "0.0.0.0",debug=True)
