19a7936cbf624fe0f10d1ac706e16d1d37d1ad4c
ayushbasak <ayushbasak0210@gmail.com>
2020-09-26 19:55:28 +0530

    Fix minor issue

--- flask-app.py
+++ flask-app.py
@@ -29,7 +29,7 @@ def home():
             dictionary['name'] = fetchedData[i][0]
             dictionary['post'] = fetchedData[i][1]
             li.append(dictionary)
-        return render_template("index-about.html",dataset=li,maxlength = 240)
+        return render_template("index-min.html",dataset=li,maxlength = 240)
     else:
         pass
     con.commit()
